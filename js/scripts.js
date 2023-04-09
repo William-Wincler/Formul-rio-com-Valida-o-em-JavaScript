1ae7eb558af8012c68c6f2426436ad8f43af3b4b
GerverSnar <williamwincler97@gmail.com>
2023-04-09 15:57:20 -0300

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const jobSelect = document.querySelector("job")
+const messageTextarea = document.querySelector("#message")
+
+form addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Verifica se o nome está vazio
+    if(nameInput.value === "") {
+        alert("Por favor, preencha o seu nome");
+        return;
+    }
+
+    // Verifica se o e-mail está preenchido e se é válido
+    if(emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("Por favor, preencha o seu e-mail");
+        return;
+    }
+
+    // Verifica se está preenchido
+    if(!validatePassword(passwordInput.value, 8)) {
+        alert("A senha precisa ter no mínimo 8 dígitos.");
+        return;
+    }
+
+    // Verificar se a siuação foi selecionada
+    if(jobSelect.value === "") {
+        alert("Por favor, selecione a sua situação");
+        return;
+    }
+
+    // Verifica se a mensagem está preenchida
+    if(messageTextarea.value === "") {
+        alert("Por favor, escreva uma mensagem");
+        return;
+    } 
+
+    // Se todos os campos estiverem corretamente preenchidos, envie o form
+form.submit()    
+});
+
+    // Função que valida e-mail
+function isEmailValid(email) {
+    // Criar uma ReGex para validar e-mail
+    const emailRegex = new RegExp( 
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emailRegex.test(email)) {
+        return true;
+    }
+
+    return false;
+} 
+
+// Função que valida a senha
+function validatePassword(password, minDigits) {
+    if(password.length >= minDigits) {
+        // Senha válida
+        return true
+    }
+
+    // Senha inválida
+    return false 
+}
